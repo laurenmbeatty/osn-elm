ac838a5d6443c2affadb2b51ac1d5f73e9ceb45b
Lauren Beatty <lauren.m.beatty@gmail.com>
2018-04-14 06:36:45 -0500

    get a start on creating model, view and update

--- PhotoGallery.elm
+++ PhotoGallery.elm
@@ -1,22 +1,62 @@
 module PhotoGallery exposing (..)
 import Html exposing (..)
---import Html.Attributes exposing (..)
+import Html.Attributes exposing (..)
 --import Html.Events exposing (onClick)
 
-initialModel : { introText : String }
-initialModel =
-  {
-  introText = "hello"
+-- MODEL
+
+type alias Model =
+  { query : String
+  , results : List SearchResult
   }
 
+type alias SearchResult =
+  { userName : String
+  , likes : Int
+  , photoUrl : String
+  }
 
-view: a -> Html msg
-view model =
-  h1 [] [text initialModel.introText]
+type Msg
+  = SetQuery String
 
+initialModel : Model
+initialModel =
+  { query = "blah"
+  , results =
+    [ { userName = "Lauren"
+      , likes = 5
+      , photoUrl = "blah"
+      }
+    ]
+  }
 
+ -- UPDATE
 update msg model =
     model
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div [ class "smallgrid even" ] (List.map viewSearchResult model.results)
+    
+
+
+viewSearchResult : SearchResult -> Html Msg
+viewSearchResult result =
+  div [ class "description" ]
+    [ div [ class "text-holder" ]
+        [ h3 []
+            [ text "Photographer:" ]
+        , h2 []
+            [ text result.userName ]
+        , p []
+            [ text "[Likes: " ++ (toString result.likes) ++ "]" ]
+        ]
+        ,
+      div [ class "photo" ]
+        [ img [ src result.photoUrl ] []
+        ]
+    ]
 
 
 main =
