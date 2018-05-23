f1c3bc303a8af0b85b1ea720158edafeafc748d9
Lauren Beatty <lauren.m.beatty@gmail.com>
2018-05-22 19:03:09 -0500

    blah

--- elm.js
+++ elm.js
@@ -9449,7 +9449,7 @@ var _laurenmbeatty$osn_elm$PhotoGallery$update = F2(
 							model,
 							{
 								results: {ctor: '[]'},
-								errorMessage: _elm_lang$core$Maybe$Just('Oops, something went wrong!')
+								errorMessage: _elm_lang$core$Maybe$Just('Oops, something went wrong.')
 							}),
 						_1: _elm_lang$core$Platform_Cmd$none
 					};
